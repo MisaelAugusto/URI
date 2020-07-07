62adc9531780291946013db7f304d4feeff6a04a
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2020-07-07 16:21:56 -0300

    Modify 1183.py script

--- Python3/1183.py
+++ Python3/1183.py
@@ -7,7 +7,7 @@ for i in range(144):
   N = float(input())
 
   line = i // 12
-  if (i < (13 * line)):
+  if (i > (13 * line)):
     total += N
 
 answer = total if (operation == 'S') else (total / 66)
